081098762fb91332d414b6e0d91f62664b7cd856
maikofelix47 <maikofelix89@gmail.com>
2023-02-27 21:04:22 +0300

    Added counter store, actions and reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { todosReducer } from './todos/todo.reducers';
 import { TodosComponent } from './todos/todos.component';
+import { CounterComponent } from './counter/counter.component';
+import { counterReducer } from './counter/counter.reducer';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     StoreModule.forRoot({
-      todos: todosReducer
+      todos: todosReducer,
+      count: counterReducer
     })
   ],
   providers: [],
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { increment, decrement, reset } from './counter.actions';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent {
+
+  count$!: Observable<number>;
+
+  constructor(private store: Store<{count: number}>){
+    this.count$ = this.store.select('count');
+  }
+
+  increment(){
+     this.store.dispatch(increment());
+  }
+
+  decrement(){
+    this.store.dispatch(decrement());
+  }
+
+  reset(){
+     this.store.dispatch(reset());
+  }
+
+}
